6c35f95db99b32a52d48aee7d7b8ed7ccb08a03d
JoaoLlucaxs <queirozjoaolucas8@gmail.com>
2024-07-04 22:29:06 -0300

    ..

---
+++ aula2/conversaoBasica.py
@@ -0,0 +1,15 @@
+# é NECESSÁRIO ESSA CONVERSÃO (INT)? -> NÃO , POIS ELE ENTENDERÁ
+#Qual é o método para imprimir Hello World ou qualquer mensagem?
+#Você pode usar os seguintes métodos –
+
+#método print()
+#Método sys.stdout.write() importando o módulo sys
+#Usando string python-f
+
+a=int(input('Digite um numero'));
+b=int((input('Digite outro numero')));
+
+total=a + b;
+print('A soma é : ' , total);
+print('Valor do a = ' , a);
+print('Valor do b = ' , b);
