1c9235be4a4639bf1267ae14b549ac92f53b8543
JoaoLlucaxs <queirozjoaolucas8@gmail.com>
2024-07-04 21:50:13 -0300

    ..

---
+++ Aula1/OlaMundo.py
@@ -0,0 +1,6 @@
+nome=input('Qual teu nome? ');
+idade=input('Qual tua idade? ');
+profissao=input('Qual tua profissao? ');
+
+print(nome,idade,profissao);
+
---
+++ Aula1/desafio.py
@@ -0,0 +1,14 @@
+nome=input('Informe seu nome');
+print("Bem vindo ao mundo Python " + nome);
+
+dia=input('Informe o dia');
+mes=input('Informe o mes');
+ano=input('Informe o ano');
+
+print('VocÊ nasceu no dia ' , dia , mes , ano ,  ' correto?');
+
+numero1=3;
+numero2=6;
+total=numero1 + numero2;
+
+print('A soma é ' , total);
