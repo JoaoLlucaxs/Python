c95c7e76792b1090b9f5e16acdb3dddfc19e9039
JoaoLlucaxs <queirozjoaolucas8@gmail.com>
2024-10-27 21:12:19 -0300

    ..

---
+++ Aula7/Casting.py
@@ -0,0 +1,22 @@
+
+
+# COMO EM QUALQUER LINGUAGEM , CASTING É A CONVERSÃO DE UM TIPO DE DADO PARA OUTRO
+
+"""
+int() - constrói um número inteiro a partir de um literal inteiro, um literal float (removendo todos os decimais) ou um literal de string (desde que a string represente um número inteiro)
+float() - constrói um número float a partir de um literal inteiro, um literal float ou um literal de string (desde que a string represente um float ou um inteiro)
+str() - constrói uma string a partir de uma ampla variedade de tipos de dados, incluindo strings, literais inteiros e literais float
+"""
+
+x = int(1)   # x will be 1
+y = int(2.8) # y will be 2
+z = int("3") # z will be 3
+
+x = float(1)     # x will be 1.0
+y = float(2.8)   # y will be 2.8
+z = float("3")   # z will be 3.0
+w = float("4.2") # w will be 4.2
+
+x = str("s1") # x will be 's1'
+y = str(2)    # y will be '2'
+z = str(3.0)  # z will be '3.0'
---
+++ Aula7/numeroRandomico.py
@@ -0,0 +1,6 @@
+import random
+
+# PYTHON NÃO TEM UM OBJETO(FUNÇÃO) QUE CRIA NUMEROS RANDOMICOS , TEM UM MODULO INTERNO QUE PODE CRIAR NUMEROS ALEATORIOS
+
+criaNumeroAleatorio=random.randrange(1,9);
+print(criaNumeroAleatorio)
